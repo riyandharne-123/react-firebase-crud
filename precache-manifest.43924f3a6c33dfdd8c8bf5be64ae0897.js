65be9710fa39e05af8040e35033715e32fced359
riyandharne-123 <rockydods123@gmail.com>
2020-05-26 10:47:35 +0530

    Updates

---
+++ precache-manifest.43924f3a6c33dfdd8c8bf5be64ae0897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69791def4fe9e3322e3c78ab50e41b21",
+    "url": "/react-firebase-crud/index.html"
+  },
+  {
+    "revision": "3e345bef287c36b726ca",
+    "url": "/react-firebase-crud/static/css/main.8fd1944a.chunk.css"
+  },
+  {
+    "revision": "363293b3a6846782b11d",
+    "url": "/react-firebase-crud/static/js/2.aca33f6c.chunk.js"
+  },
+  {
+    "revision": "2954b1568600ec1d7b70d079ce9ac1a4",
+    "url": "/react-firebase-crud/static/js/2.aca33f6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e345bef287c36b726ca",
+    "url": "/react-firebase-crud/static/js/main.5c88d9e7.chunk.js"
+  },
+  {
+    "revision": "88d28217670c15d6adf5",
+    "url": "/react-firebase-crud/static/js/runtime-main.46303fd4.js"
+  }
+]);
